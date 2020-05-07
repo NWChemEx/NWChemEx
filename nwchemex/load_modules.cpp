f9e477d12ccd143c1213e6b76f54f94406847d72
jboschen <jboschen@iastate.edu>
2020-05-07 16:04:06 -0500

    works with current build and repos using TA

--- nwchemex/load_modules.cpp
+++ nwchemex/load_modules.cpp
@@ -1,13 +1,18 @@
 #include "nwchemex/load_modules.hpp"
 #include <integrals/integralsmm.hpp>
 #include <scf/scf_mm.hpp>
+#include <property_types/ao_integrals/kinetic.hpp>
+#include <property_types/ao_integrals/nuclear.hpp>
 
 namespace nwx {
 
 void load_modules(sde::ModuleManager& mm) {
-    //SCF right now includes this, uncomment when fixed
-    //integrals::libint::load_modules(mm);
+    integrals::load_modules(mm);
     scf::load_modules(mm);
+
+    // TODO: move these defaults to the Integrals repo
+    mm.set_default<property_types::KineticIntegral<double>>(std::string{"Kinetic"});
+    mm.set_default<property_types::NuclearIntegral<double>>(std::string{"Nuclear"});
 }
 
 } // namespace nwx
