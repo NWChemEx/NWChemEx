bbbd69347307e800ec5a0282cd4a1f50a199aae4
Ryan Richard <ryanmrichard1@gmail.com>
2020-07-19 14:21:33 -0500

    adds doxygen to the workflow

--- nwchemex/load_modules.cpp
+++ nwchemex/load_modules.cpp
@@ -1,18 +1,25 @@
 #include "nwchemex/load_modules.hpp"
 #include <integrals/integralsmm.hpp>
+#include <mp2/mp2_mm.hpp>
 #include <scf/scf_mm.hpp>
 #include <property_types/ao_integrals/kinetic.hpp>
 #include <property_types/ao_integrals/nuclear.hpp>
+
+namespace {
+
+// Sets the project-wide defaults for modules in the SCF repo
+void scf_defaults(sde::ModuleManager& mm) {
+
+}
+
+}
 
 namespace nwx {
 
 void load_modules(sde::ModuleManager& mm) {
     integrals::load_modules(mm);
     scf::load_modules(mm);
-
-    // TODO: move these defaults to the Integrals repo
-    mm.set_default<property_types::KineticIntegral<double>>(std::string{"Kinetic"});
-    mm.set_default<property_types::NuclearIntegral<double>>(std::string{"Nuclear"});
+    mp2::load_modules(mm);
 }
 
 } // namespace nwx
