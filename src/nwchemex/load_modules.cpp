24538dd9d6cdf117766e798dcfed53c989ae781e
ryan <ryanmrichard1@gmail.com>
2021-08-19 12:00:05 -0500

    mp2 energy works again

--- src/nwchemex/load_modules.cpp
+++ src/nwchemex/load_modules.cpp
@@ -1,7 +1,7 @@
 #include "modules.hpp"
 #include "nwchemex/load_modules.hpp"
 #include <integrals/integrals.hpp>
-//#include <mp2/mp2_mm.hpp>
+#include <mp2/mp2.hpp>
 #include <scf/scf.hpp>
 
 namespace {
@@ -20,21 +20,25 @@ void set_scf_default_modules(pluginplay::ModuleManager& mm) {
     // mm.change_submod("SCFDIIS", "S Builder", "Overlap");
 }
 
-// void set_mp2_default_modules(sde::ModuleManager& mm) {
-//     mm.change_submod("DOI SparseMap Builder", "dois", "DOI");
-//     mm.change_submod("LMO 2 AO", "dois", "DOI");
-//     mm.change_submod("AO 2 Aux", "dois", "DOI");
-//     mm.change_submod("DLPNO", "Fock Builder", "Fock");
-//     mm.change_submod("Dipole Center", "Dipole Builder", "EDipole");
-//     mm.change_submod("Transition Dipole", "Dipole Builder", "EDipole");
-//     mm.change_submod("MP2 3-Center K", "(P | mu nu)", "ERI3");
-//     mm.change_submod("MP2 3-Center K", "(Q | P)", "MetricChol");
-//     mm.change_submod("MP2 3-Center Sparse K", "(Q | P)", "Sparse DF Coefs");
-//     mm.change_submod("MP2 3-Center Sparse K", "(P | mu nu)", "ERI3");
-//     mm.change_submod("MP2 4-Center K", "(mu nu | lambda sigma)", "ERI4");
-//     mm.change_submod("MP2 4-Center Sparse K", "(mu nu | lambda sigma)",
-//     "ERI4"); mm.change_submod("Sparse DF Coefs", "M Builder", "ERI2");
-// }
+void set_mp2_default_modules(pluginplay::ModuleManager& mm) {
+    mm.change_submod("MP1 Wavefunction", "Transformed ERIS",
+                     "Transformed ERI4");
+    mm.change_submod("MP2", "Transformed ERIs", "Transformed ERI4");
+    //     mm.change_submod("DOI SparseMap Builder", "dois", "DOI");
+    //     mm.change_submod("LMO 2 AO", "dois", "DOI");
+    //     mm.change_submod("AO 2 Aux", "dois", "DOI");
+    //     mm.change_submod("DLPNO", "Fock Builder", "Fock");
+    //     mm.change_submod("Dipole Center", "Dipole Builder", "EDipole");
+    //     mm.change_submod("Transition Dipole", "Dipole Builder", "EDipole");
+    //     mm.change_submod("MP2 3-Center K", "(P | mu nu)", "ERI3");
+    //     mm.change_submod("MP2 3-Center K", "(Q | P)", "MetricChol");
+    //     mm.change_submod("MP2 3-Center Sparse K", "(Q | P)", "Sparse DF
+    //     Coefs"); mm.change_submod("MP2 3-Center Sparse K", "(P | mu nu)",
+    //     "ERI3"); mm.change_submod("MP2 4-Center K", "(mu nu | lambda sigma)",
+    //     "ERI4"); mm.change_submod("MP2 4-Center Sparse K", "(mu nu | lambda
+    //     sigma)", "ERI4"); mm.change_submod("Sparse DF Coefs", "M Builder",
+    //     "ERI2");
+}
 
 } // namespace
 
@@ -43,12 +47,12 @@ namespace nwx {
 void load_modules(pluginplay::ModuleManager& mm) {
     integrals::load_modules(mm);
     scf::load_modules(mm);
-    // mp2::load_modules(mm);
+    mp2::load_modules(mm);
 
     mm.add_module<SystemHamiltonian>("SystemHamiltonian");
 
     set_scf_default_modules(mm);
-    // set_mp2_default_modules(mm);
+    set_mp2_default_modules(mm);
 }
 
 } // namespace nwx
--- tests/mp2/mp2.cpp
+++ tests/mp2/mp2.cpp
@@ -1,21 +1,26 @@
-// #include <catch2/catch.hpp>
-// #include <libchemist/libchemist.hpp>
-// #include <nwchemex/load_modules.hpp>
-// #include <property_types/correlation_energy.hpp>
-// #include <property_types/reference_wavefunction.hpp>
+#include "nwchemex/nwchemex.hpp"
+#include <catch2/catch.hpp>
+#include <mokup/mokup.hpp>
 
-// using canonical_mos = property_types::type::canonical_space_t<double>;
-// using scf_pt = property_types::ReferenceWavefunction<double, canonical_mos>;
-// using mp2_pt = property_types::CorrelationEnergy<double, canonical_mos>;
+using scf_wf_pt = simde::CanonicalReference;
+using mp1_wf_pt = simde::CanonicalManyBodyWf;
+using mp2_e_pt  = simde::CanonicalCorrelationEnergy;
 
-// TEST_CASE("Canonical MP2") {
-//     sde::ModuleManager mm;
-//     nwx::load_modules(mm);
-//     libchemist::MoleculeManager mols;
+TEST_CASE("Canonical MP2") {
+    auto& world = TA::get_default_world();
+    pluginplay::ModuleManager mm;
+    nwx::load_modules(mm);
 
-//     auto mol              = mols.at("water");
-//     auto bs               = libchemist::apply_basis("cc-pvdz", mol);
-//     const auto [E_SCF, C] = mm.run_as<scf_pt>("SCFDIIS", mol, bs);
-//     const auto [E_MP2]    = mm.run_as<mp2_pt>("MP2", mol, bs, C);
-//     std::cout << E_MP2 << std::endl;
-// }
+    const auto name = mokup::molecule::h2o;
+    const auto bs   = mokup::basis_set::sto3g;
+    auto mol        = mokup::get_molecules().at(name);
+    auto aos        = mokup::get_bases().at(name).at(bs);
+    auto H          = mokup::hamiltonian(name);
+    simde::type::els_hamiltonian H_e(H);
+    auto scf_wf_mod = mm.at("SCF");
+    auto mp1_wf_mod = mm.at("MP1 Wavefunction");
+    auto [scf_wf]   = scf_wf_mod.run_as<scf_wf_pt>(H_e, aos);
+    auto [mp1_wf]   = mp1_wf_mod.run_as<mp1_wf_pt>(H_e, scf_wf);
+    auto [E]        = mm.run_as<mp2_e_pt>("MP2", scf_wf, H_e, mp1_wf);
+    std::cout << E << std::endl;
+}
