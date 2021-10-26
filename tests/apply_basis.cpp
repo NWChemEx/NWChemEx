fe95fb4e4898eddf52c40a42c270e5a276be4dee
Clang Robot <robot@example.com>
2021-10-26 17:32:17 +0000

    Committing clang-format changes

--- tests/apply_basis.cpp
+++ tests/apply_basis.cpp
@@ -11,14 +11,14 @@ using coord_type = std::array<double, 3>;
 inline auto corr_bs() {
     AOBasisSet<double> rv;
     auto man = chemcache::nwx_basis_set_manager();
-    auto O = man.get_basis("sto-3g", 8);
+    auto O   = man.get_basis("sto-3g", 8);
     for(auto i : {0, 1, 2}) O.coord(i) = mol[0].coords()[i];
     rv.add_center(O);
-    
+
     auto H1 = man.get_basis("sto-3g", 1);
     for(auto i : {0, 1, 2}) H1.coord(i) = mol[1].coords()[i];
     rv.add_center(H1);
-    
+
     auto H2 = man.get_basis("sto-3g", 1);
     for(auto i : {0, 1, 2}) H2.coord(i) = mol[2].coords()[i];
     rv.add_center(H2);
