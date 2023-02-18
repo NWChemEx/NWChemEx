9abae93d90dce9d9164c553ccc52849e0d4e467f
Jonathan M. Waldrop <relik107@gmail.com>
2023-02-18 08:47:36 -0800

    backup

--- end_to_end_driver/driver.cpp
+++ end_to_end_driver/driver.cpp
@@ -33,9 +33,9 @@ int main(int argc, char* argv[]) {
     mm.change_input("DFK", "Fitting Basis", aux);
     if(have_gpu_modules) {
         mm.change_input("DFJ_JEngine", "Fitting Basis", aux);
-        // mm.change_input("GauXC Quadrature Batches", "On GPU", true);
-        // mm.change_input("snLinK", "On GPU", true);
-        // mm.change_input("XC", "On GPU", true);
+        mm.change_input("GauXC Quadrature Batches", "On GPU", true);
+        mm.change_input("snLinK", "On GPU", true);
+        mm.change_input("XC", "On GPU", true);
     }
 
     /// Lambda module to determine the shapes based on system
