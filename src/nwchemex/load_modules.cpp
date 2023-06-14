25e396d1ec69d0ccfc928972a7367b56583dfd88
Kevin Gasperich <kgasperich@gmail.com>
2023-06-14 16:46:27 -0500

    updated for new MOs module

--- src/nwchemex/load_modules.cpp
+++ src/nwchemex/load_modules.cpp
@@ -37,11 +37,8 @@ void set_scf_default_modules(pluginplay::ModuleManager& mm) {
     mm.change_submod("MetricChol", "M Builder", "ERI2");
     mm.change_submod("CoreH", "Kinetic Energy", "Kinetic");
     mm.change_submod("CoreH", "Electron-Nuclear Attraction", "Nuclear");
-    mm.change_submod("CoreGuess", "Overlap", "Overlap");
-    mm.change_submod("SADGuess", "Overlap", "Overlap");
-    mm.change_submod("SCF Step", "Overlap", "Overlap");
+    mm.change_submod("MOs Fock", "Overlap", "Overlap");
     mm.change_submod("DIIS Fock Matrix", "Overlap", "Overlap");
-    mm.change_submod("SCFDIIS Step", "Overlap", "Overlap");
     mm.change_submod("XC", "Tensor Shape", "OneTileShape");
 }
 
