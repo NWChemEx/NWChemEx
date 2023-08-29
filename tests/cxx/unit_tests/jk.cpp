737e647471a396e2f85520309578595a8d9011c0
Kevin Gasperich <kgasperich@gmail.com>
2023-08-29 18:16:41 -0500

    testing df

--- tests/cxx/unit_tests/jk.cpp
+++ tests/cxx/unit_tests/jk.cpp
@@ -144,6 +144,9 @@ TEST_CASE("JK mixed") {
 }
 
 TEST_CASE("DFJK mixed") {
+    auto t0      = std::chrono::high_resolution_clock::now();
+    auto t1      = std::chrono::high_resolution_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
     pluginplay::ModuleManager mm;
     nwchemex::load_modules(mm);
     mm.change_submod("DFK", "ERI Builder", "Transformed ERI3");
@@ -194,80 +197,80 @@ TEST_CASE("DFJK mixed") {
     inputs.at("k̂").change(k0);
     inputs.at("Fitting Basis").change(aux_aos);
 
-    std::cout << "J01 from DFJ: ";
-    auto t0      = std::chrono::high_resolution_clock::now();
-    auto J01_dfj = dfj_mod.run_as<j_pt>(aos1, j0, aos1);
-    auto t1      = std::chrono::high_resolution_clock::now();
-    auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
-
-    std::cout << "K01 from DFK: ";
-    t0           = std::chrono::high_resolution_clock::now();
-    auto K01_dfk = dfk_mod.run_as<k_pt>(aos1, k0, aos1);
-    t1           = std::chrono::high_resolution_clock::now();
-    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
-
-    std::cout << "J01 from DFJK: ";
+    std::cout << "J01 from DFJK: \n";
     t0                 = std::chrono::high_resolution_clock::now();
     std::tie(J01_dfjk) = j_pt::unwrap_results(dfjk_mod.run(inputs));
     t1                 = std::chrono::high_resolution_clock::now();
     dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
+    // std::cout << "J01 from DFJK: " << dt.count() << " ms\n";
 
-    std::cout << "K01 from DFJK: ";
+    std::cout << "K01 from DFJK: \n";
     t0                 = std::chrono::high_resolution_clock::now();
     std::tie(K01_dfjk) = k_pt::unwrap_results(dfjk_mod.run(inputs));
     t1                 = std::chrono::high_resolution_clock::now();
     dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
+    // std::cout << "K01 from DFJK: " << dt.count() << " ms\n";
+
+    std::cout << "J01 from DFJ: \n";
+    t0      = std::chrono::high_resolution_clock::now();
+    auto J01_dfj = dfj_mod.run_as<j_pt>(aos1, j0, aos1);
+    t1      = std::chrono::high_resolution_clock::now();
+    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    // std::cout << "J01 from DFJ: " << dt.count() << " ms\n";
+
+    std::cout << "K01 from DFK: \n";
+    t0           = std::chrono::high_resolution_clock::now();
+    auto K01_dfk = dfk_mod.run_as<k_pt>(aos1, k0, aos1);
+    t1           = std::chrono::high_resolution_clock::now();
+    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    // std::cout << "K01 from DFK: " << dt.count() << " ms\n";
 
     REQUIRE(tensorwrapper::tensor::allclose(J01_dfjk, J01_dfj));
-    REQUIRE(tensorwrapper::tensor::allclose(K01_dfjk, K01_dfk));
+    // REQUIRE(tensorwrapper::tensor::allclose(K01_dfjk, K01_dfk));
 
-    // build J/K in aos0 from rho0
-    simde::type::tensor J00_dfjk, K00_dfjk;
-    inputs.at("bra").change(aos1);
-    inputs.at("ket").change(aos1);
-    inputs.at("(r̂₁₂)⁻¹").change(j0);
-    inputs.at("k̂").change(k0);
+    // // build J/K in aos0 from rho0
+    // simde::type::tensor J00_dfjk, K00_dfjk;
+    // inputs.at("bra").change(aos1);
+    // inputs.at("ket").change(aos1);
+    // inputs.at("(r̂₁₂)⁻¹").change(j0);
+    // inputs.at("k̂").change(k0);
 
-    std::cout << "J00 from DFJK: ";
-    t0                 = std::chrono::high_resolution_clock::now();
-    std::tie(J00_dfjk) = j_pt::unwrap_results(dfjk_mod.run(inputs));
-    t1                 = std::chrono::high_resolution_clock::now();
-    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
+    // std::cout << "J00 from DFJK: \n";
+    // t0                 = std::chrono::high_resolution_clock::now();
+    // std::tie(J00_dfjk) = j_pt::unwrap_results(dfjk_mod.run(inputs));
+    // t1                 = std::chrono::high_resolution_clock::now();
+    // dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    // // std::cout << "J00 from DFJK: " << dt.count() << " ms\n";
 
-    std::cout << "K00 from DFJK: ";
-    t0                 = std::chrono::high_resolution_clock::now();
-    std::tie(K00_dfjk) = k_pt::unwrap_results(dfjk_mod.run(inputs));
-    t1                 = std::chrono::high_resolution_clock::now();
-    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
+    // std::cout << "K00 from DFJK: \n";
+    // t0                 = std::chrono::high_resolution_clock::now();
+    // std::tie(K00_dfjk) = k_pt::unwrap_results(dfjk_mod.run(inputs));
+    // t1                 = std::chrono::high_resolution_clock::now();
+    // dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    // // std::cout << "K00 from DFJK: " << dt.count() << " ms\n";
 
-    std::cout << "J00 from DFJ: ";
-    t0           = std::chrono::high_resolution_clock::now();
-    auto J00_dfj = dfj_mod.run_as<j_pt>(aos0, j0, aos0);
-    t1           = std::chrono::high_resolution_clock::now();
-    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
+    // std::cout << "J00 from DFJ: \n";
+    // t0           = std::chrono::high_resolution_clock::now();
+    // auto J00_dfj = dfj_mod.run_as<j_pt>(aos0, j0, aos0);
+    // t1           = std::chrono::high_resolution_clock::now();
+    // dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    // // std::cout << "J00 from DFJ: " << dt.count() << " ms\n";
 
-    std::cout << "K01 from DFK: ";
-    t0           = std::chrono::high_resolution_clock::now();
-    auto K00_dfk = dfk_mod.run_as<k_pt>(aos0, k0, aos0);
-    t1           = std::chrono::high_resolution_clock::now();
-    dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
-    std::cout << dt.count() << " ms\n";
+    // std::cout << "K01 from DFK: \n";
+    // t0           = std::chrono::high_resolution_clock::now();
+    // auto K00_dfk = dfk_mod.run_as<k_pt>(aos0, k0, aos0);
+    // t1           = std::chrono::high_resolution_clock::now();
+    // dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+    // // std::cout << "K01 from DFK: " << dt.count() << " ms\n";
 
-    REQUIRE(tensorwrapper::tensor::allclose(J00_dfjk, J00_dfj));
-    REQUIRE(tensorwrapper::tensor::allclose(K00_dfjk, K00_dfk));
+    // REQUIRE(tensorwrapper::tensor::allclose(J00_dfjk, J00_dfj));
+    // REQUIRE(tensorwrapper::tensor::allclose(K00_dfjk, K00_dfk));
 
     // TODO: remove this before merge
-    std::cout << "DFJ.profile_info()\n";
-    std::cout << dfj_mod.profile_info() << std::endl;
-    std::cout << "DFK.profile_info()\n";
-    std::cout << dfk_mod.profile_info() << std::endl;
-    std::cout << "\nDFJK.profile_info()\n";
-    std::cout << dfjk_mod.profile_info() << std::endl;
+    // std::cout << "DFJ.profile_info()\n";
+    // std::cout << dfj_mod.profile_info() << std::endl;
+    // std::cout << "DFK.profile_info()\n";
+    // std::cout << dfk_mod.profile_info() << std::endl;
+    // std::cout << "\nDFJK.profile_info()\n";
+    // std::cout << dfjk_mod.profile_info() << std::endl;
 }
