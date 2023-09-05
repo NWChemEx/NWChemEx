5f7e70e46e9542253c7d7dfbfae31ca202796d3d
David Williams-Young <dbwy@lbl.gov>
2023-09-05 14:37:20 -0700

    Updates

--- end_to_end_driver/driver.cpp
+++ end_to_end_driver/driver.cpp
@@ -4,6 +4,44 @@
 using mol_bs_pt   = simde::MolecularBasisSet;
 using molecule_pt = simde::MoleculeFromString;
 
+template <typename ZMod, typename AMod>
+chemist::Molecule from_xyz(ZMod&& symb_to_z, AMod&& atom_from_z, 
+  std::string fname) {
+
+  std::cout << "Reading from XYZ File " << fname << std::endl;
+
+  std::ifstream xyz_file(fname);
+  chemist::Molecule mol;
+
+  std::string line;
+  std::getline(xyz_file, line); // Atom count
+  std::getline(xyz_file, line); // Comment line
+
+  while(std::getline(xyz_file, line)) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::stringstream ss{line};
+    while(std::getline(ss, token, ' ')) {
+        if(token.size()) {
+            tokens.emplace_back(token);
+        }
+    }
+    auto Z = symb_to_z.template run_as<simde::ZFromSymbol>(tokens.at(0));
+    auto x = std::stod(tokens.at(1));
+    auto y = std::stod(tokens.at(2));
+    auto z = std::stod(tokens.at(3));
+
+    simde::type::atom atm = atom_from_z.template run_as<simde::AtomFromZ>(Z);
+    atm.x() = x;
+    atm.y() = y;
+    atm.z() = z;
+    mol.push_back(atm);
+    
+  }
+
+  return mol;
+}
+
 int main(int argc, char* argv[]) {
     auto& world = TA::initialize(argc, argv);
 
@@ -24,7 +62,9 @@ int main(int argc, char* argv[]) {
     bool have_gpu_modules = mm.count("DFJ_JEngine");
 
     /// Job specifications
-    auto mol  = mm.at("NWX Molecules").run_as<molecule_pt>(mol_name);
+    auto mol  = mol_name.find(".xyz") == std::string::npos ? 
+                mm.at("NWX Molecules").run_as<molecule_pt>(mol_name) :
+                from_xyz(mm.at("Z from Symbol"), mm.at("Atom"), mol_name);
     auto obs  = mm.at(basis_name).run_as<mol_bs_pt>(mol);
     auto axbs = mm.at(aux_basis_name).run_as<mol_bs_pt>(mol);
 
@@ -43,7 +83,7 @@ int main(int argc, char* argv[]) {
     mm.change_input("DFJ", "Fitting Basis", aux);
     mm.change_input("DFK", "Fitting Basis", aux);
     mm.change_submod("Fock Matrix", "J Builder", "DFJ");
-    mm.change_submod("Fock Matrix", "K Builder", "DFK");
+    //mm.change_submod("Fock Matrix", "K Builder", "DFK");
 
     // Set screening thresholds
     mm.change_input("Overlap CS", "Screening Threshold", 1e-12);
@@ -60,11 +100,13 @@ int main(int argc, char* argv[]) {
     mm.change_submod("Nuclear", "Tensor Shape", "CenterTiledShape");
     mm.change_submod("Kinetic", "Tensor Shape", "CenterTiledShape");
     mm.change_submod("Overlap", "Tensor Shape", "CenterTiledShape");
+    mm.change_submod("XC", "Tensor Shape", "CenterTiledShape");
 
     // Use gpu modules
     if(have_gpu_modules) {
         mm.change_input("GauXC Quadrature Batches", "On GPU", true);
         mm.change_input("snLinK", "On GPU", true);
+        mm.change_input("snLinK", "Integration Grid", "Fine");
         mm.change_input("XC", "On GPU", true);
         mm.change_input("DFJ_JEngine", "Fitting Basis", aux);
         mm.change_submod("Fock Matrix", "J Builder", "DFJ_JEngine");
@@ -74,21 +116,29 @@ int main(int argc, char* argv[]) {
     // Use CS screened integrals
     mm.change_submod("CoreH", "Electron-Nuclear Attraction", "Nuclear CS");
     mm.change_submod("CoreH", "Kinetic Energy", "Kinetic CS");
-    mm.change_submod("CoreGuess", "Overlap", "Overlap CS");
-    mm.change_submod("SCF Step", "Overlap", "Overlap CS");
+    mm.change_submod("MOs Fock", "Overlap", "Overlap CS");
+    //mm.change_submod("SCF Step", "Overlap", "Overlap CS");
     mm.change_submod("DIIS Fock Matrix", "Overlap", "Overlap CS");
-    mm.change_submod("SCFDIIS Step", "Overlap", "Overlap CS");
+    //mm.change_submod("SCFDIIS Step", "Overlap", "Overlap CS");
 
     // Use DIIS
     mm.change_submod("SCF Energy", "Reference Wave Function",
                      "SCFDIIS Wavefunction");
 
+    // Use SAD
+    //mm.change_submod("SADDensity", "Atomic Density", "sto-3g atomic dm");
+    //mm.change_submod("SCFDIIS Wavefunction", "Guess", "SADGuess");
+
     // Run and print profile info
     auto E = mm.at("SCF Energy").run_as<simde::AOEnergy>(aos, cs);
     world.gop.fence();
+    std::cout << std::scientific << std::setprecision(16);
     std::cout << "SCF Energy = " << E << std::endl;
-    std::cout << "SCF Profile:" << std::endl;
-    std::cout << mm.at("SCF Energy").profile_info() << std::endl;
+    //std::cout << "SCF Profile:" << std::endl;
+    //for(auto& [name, mod] : mm) {
+    //  std::cout << name << " " << mod->profile_info(false) << std::endl;
+    //}
+    //std::cout << mm.at("SCF Energy").profile_info() << std::endl;
 
     TA::finalize();
     return 0;
